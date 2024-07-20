7b3680b2a63f48d38ba39dab31ba1c8bca136e00
Jincy kp <jincykp98@gmail.com>
2024-07-20 12:05:51 +0530

    search done

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,206 @@
+// import 'package:cached_network_image/cached_network_image.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:netflix_clone_s/commom/utils.dart';
+// import 'package:netflix_clone_s/models/movie_recommendation_model.dart';
+// import 'package:netflix_clone_s/models/search_model.dart';
+// import 'package:netflix_clone_s/services/api_services.dart';
+
+// class SearchScreen extends StatefulWidget {
+//   const SearchScreen({super.key});
+
+//   @override
+//   State<SearchScreen> createState() => _SearchScreenState();
+// }
+
+// class _SearchScreenState extends State<SearchScreen> {
+//   late Future<MovieRecommendationModel> popularMovies;
+//   TextEditingController searchController = TextEditingController();
+//   ApiServices apiServices = ApiServices();
+//   SearchModel? searchModel;
+//   void search(String query) {
+//     apiServices.getSearchedMovie(query).then((results) {
+//       setState(() {
+//         searchModel = results;
+//       });
+//     });
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     popularMovies = apiServices.getPopularMovies();
+//   }
+
+//   @override
+//   void dispose() {
+//     searchController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SafeArea(
+//       child: Scaffold(
+//           body: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: SingleChildScrollView(
+//           child: SingleChildScrollView(
+//             child: Column(
+//               children: [
+//                 CupertinoSearchTextField(
+//                   controller: searchController,
+//                   prefixIcon: const Icon(
+//                     Icons.search,
+//                     color: Colors.grey,
+//                   ),
+//                   suffixIcon: const Icon(
+//                     Icons.cancel,
+//                     color: Colors.grey,
+//                   ),
+//                   style: const TextStyle(color: Colors.white),
+//                   backgroundColor: Colors.grey.withOpacity(0.3),
+//                   onChanged: (value) {
+//                     if (value.isEmpty) {
+//                     } else {
+//                       search(searchController.text);
+//                     }
+//                   },
+//                 ),
+//                 searchController.text.isEmpty
+//                     ? FutureBuilder(
+//                         future: popularMovies,
+//                         builder: (context, snapshot) {
+//                           if (snapshot.connectionState ==
+//                               ConnectionState.waiting) {
+//                             return const Center(
+//                                 child: CircularProgressIndicator());
+//                           } else if (snapshot.hasError) {
+//                             return Center(
+//                                 child: Text('Error: ${snapshot.error}'));
+//                           } else if (!snapshot.hasData ||
+//                               snapshot.data!.results.isEmpty) {
+//                             return const Center(
+//                                 child: Text('No now playing movies found'));
+//                           } else {
+//                             var data = snapshot.data!.results;
+//                             return Column(
+//                               crossAxisAlignment: CrossAxisAlignment.start,
+//                               children: [
+//                                 const Text(
+//                                   "Top Search",
+//                                   style: TextStyle(
+//                                       fontWeight: FontWeight.bold,
+//                                       color: Colors.white),
+//                                 ),
+//                                 const SizedBox(
+//                                   height: 20,
+//                                 ),
+//                                 SizedBox(
+//                                   height:
+//                                       220, // Define the height for the ListView.builder
+//                                   child: ListView.builder(
+//                                     itemCount: data.length,
+//                                     physics:
+//                                         const NeverScrollableScrollPhysics(),
+//                                     scrollDirection: Axis.vertical,
+//                                     shrinkWrap: true,
+//                                     itemBuilder: (context, index) {
+//                                       return Container(
+//                                         height: 150,
+//                                         padding: const EdgeInsets.all(5),
+//                                         decoration: BoxDecoration(
+//                                           borderRadius:
+//                                               BorderRadius.circular(20),
+//                                         ),
+//                                         child: Row(
+//                                           children: [
+//                                             Image.network(
+//                                               "https://image.tmdb.org/t/p/w500${data[index].posterPath}",
+//                                               fit: BoxFit.cover,
+//                                               errorBuilder:
+//                                                   (context, error, stackTrace) {
+//                                                 return const Icon(
+//                                                   Icons.broken_image,
+//                                                   color: Colors.white,
+//                                                 );
+//                                               },
+//                                             ),
+//                                             const SizedBox(
+//                                               width: 20,
+//                                             ),
+//                                             SizedBox(
+//                                                 width: 260,
+//                                                 child: Text(
+//                                                   data[index].title,
+//                                                   maxLines: 2,
+//                                                   overflow:
+//                                                       TextOverflow.ellipsis,
+//                                                 ))
+//                                           ],
+//                                         ),
+//                                       );
+//                                     },
+//                                   ),
+//                                 ),
+//                               ],
+//                             );
+//                           }
+//                         },
+//                       )
+//                     : searchModel == null
+//                         ? const SizedBox.shrink()
+//                         : GridView.builder(
+//                             physics: const NeverScrollableScrollPhysics(),
+//                             shrinkWrap: true,
+//                             itemCount: searchModel?.results.length,
+//                             gridDelegate:
+//                                 const SliverGridDelegateWithFixedCrossAxisCount(
+//                                     crossAxisCount: 3,
+//                                     mainAxisSpacing: 15,
+//                                     crossAxisSpacing: 5,
+//                                     childAspectRatio: 1.2 / 2),
+//                             itemBuilder: (context, index) {
+//                               return Column(
+//                                 children: [
+//                                   searchModel!.results[index].backdropPath ==
+//                                           null
+//                                       ? Image.asset(
+//                                           "assets/netflix (3).png",
+//                                           height: 145,
+//                                         )
+//                                       : CachedNetworkImage(
+//                                           imageUrl:
+//                                               "https://image.tmdb.org/t/p/w500$imageUrl${searchModel!.results[index].backdropPath}",
+//                                           height: 145,
+//                                         ),
+//                                   SizedBox(
+//                                     width: 100,
+//                                     child: Text(
+//                                       searchModel!.results[index].originalTitle,
+//                                       maxLines: 2,
+//                                       overflow: TextOverflow.ellipsis,
+//                                       style: const TextStyle(fontSize: 14),
+//                                     ),
+//                                   )
+//                                 ],
+//                               );
+//                             })
+//               ],
+//             ),
+//           ),
+//         ),
+//       )),
+//     );
+//   }
+// }
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix_clone_s/commom/utils.dart';
+import 'package:netflix_clone_s/models/movie_recommendation_model.dart';
+import 'package:netflix_clone_s/models/search_model.dart';
+import 'package:netflix_clone_s/services/api_services.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,11 +210,174 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  late Future<MovieRecommendationModel> popularMovies;
+  TextEditingController searchController = TextEditingController();
+  ApiServices apiServices = ApiServices();
+  SearchModel? searchModel;
+
+  void search(String query) {
+    apiServices.getSearchedMovie(query).then((results) {
+      setState(() {
+        searchModel = results;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    popularMovies = apiServices.getPopularMovies();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("ssssome"),
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                CupertinoSearchTextField(
+                  controller: searchController,
+                  prefixIcon: const Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  ),
+                  suffixIcon: const Icon(
+                    Icons.cancel,
+                    color: Colors.grey,
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                  backgroundColor: Colors.grey.withOpacity(0.3),
+                  onChanged: (value) {
+                    if (value.isEmpty) {
+                      setState(() {
+                        searchModel = null;
+                      });
+                    } else {
+                      search(searchController.text);
+                    }
+                  },
+                ),
+                const SizedBox(height: 10),
+                searchController.text.isEmpty
+                    ? FutureBuilder<MovieRecommendationModel>(
+                        future: popularMovies,
+                        builder: (context, snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const Center(
+                                child: CircularProgressIndicator());
+                          } else if (snapshot.hasError) {
+                            return Center(
+                                child: Text('Error: ${snapshot.error}'));
+                          } else if (!snapshot.hasData ||
+                              snapshot.data!.results.isEmpty) {
+                            return const Center(
+                                child: Text('No popular movies found'));
+                          } else {
+                            var data = snapshot.data!.results;
+                            return Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Top Search",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                ListView.builder(
+                                  itemCount: data.length,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  shrinkWrap: true,
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                      height: 150,
+                                      padding: const EdgeInsets.all(5),
+                                      child: Row(
+                                        children: [
+                                          Image.network(
+                                            "https://image.tmdb.org/t/p/w500${data[index].posterPath}",
+                                            fit: BoxFit.cover,
+                                            errorBuilder:
+                                                (context, error, stackTrace) {
+                                              return const Icon(
+                                                Icons.broken_image,
+                                                color: Colors.white,
+                                              );
+                                            },
+                                          ),
+                                          const SizedBox(width: 20),
+                                          Expanded(
+                                            child: Text(
+                                              data[index].title,
+                                              maxLines: 2,
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ],
+                            );
+                          }
+                        },
+                      )
+                    : searchModel == null
+                        ? const SizedBox.shrink()
+                        : GridView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: searchModel?.results.length,
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3,
+                              mainAxisSpacing: 15,
+                              crossAxisSpacing: 5,
+                              childAspectRatio: 1.2 / 2,
+                            ),
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: [
+                                  searchModel!.results[index].backdropPath ==
+                                          null
+                                      ? Image.asset(
+                                          "assets/netflix (3).png",
+                                          height: 145,
+                                        )
+                                      : CachedNetworkImage(
+                                          imageUrl:
+                                              "https://image.tmdb.org/t/p/w500${searchModel!.results[index].backdropPath}",
+                                          height: 145,
+                                        ),
+                                  SizedBox(
+                                    width: 100,
+                                    child: Text(
+                                      searchModel!.results[index].originalTitle,
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: const TextStyle(fontSize: 14),
+                                    ),
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -2,7 +2,9 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:netflix_clone_s/commom/utils.dart';
+import 'package:netflix_clone_s/models/movie_recommendation_model.dart';
 import 'package:netflix_clone_s/models/nowPlaying_model.dart';
+import 'package:netflix_clone_s/models/search_model.dart';
 import 'package:netflix_clone_s/models/tvseries_model.dart';
 import 'package:netflix_clone_s/models/upcoming_model.dart';
 import 'package:http/http.dart' as http;
@@ -10,6 +12,8 @@ import 'package:http/http.dart' as http;
 const baseUrl = "https://api.themoviedb.org/3/";
 var key = "?api_key=$apiKey";
 late String endPoint;
+const accessToken =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NzgxNjEzZjYyNDkxNzAzMGI5Njc3NDhhNWJjYzZhYSIsIm5iZiI6MTcyMDUwMDQ1NC40NTMwOTIsInN1YiI6IjY2OGJlZTQ5NzdiYjYwNzBiNjYzNWUyYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3E7YkMlYJdA5lPrnrsOvZZS5aoysWS72JWOZC3OC6_k";
 
 class ApiServices {
   Future<UpcomingMovieModel> getUpcomingMovies() async {
@@ -82,5 +86,43 @@ class ApiServices {
       }
     }
     throw 'Faled to load top rated movies';
+  }
+
+  Future<SearchModel> getSearchedMovie(String searchtext) async {
+    endPoint = "search/movie?query=$searchtext";
+    final url = "$baseUrl$endPoint";
+    print("search url =$url");
+    final response = await http
+        .get(Uri.parse(url), headers: {"Authorization": "Bearer $accessToken"});
+
+    if (response.statusCode == 200) {
+      log('search success');
+      return SearchModel.fromJson(jsonDecode(response.body));
+    }
+    throw Exception("Failed to load searched movies ");
+  }
+
+  Future<MovieRecommendationModel> getPopularMovies() async {
+    int attempts = 0;
+    const int maxAttempts = 5;
+
+    while (attempts < maxAttempts) {
+      try {
+        String endUrl = 'movie/popular';
+        final url = '$baseUrl$endUrl$key';
+        final response = await http.get(Uri.parse(url));
+        log(response.statusCode.toString());
+        if (response.statusCode == 200) {
+          return MovieRecommendationModel.fromJson(jsonDecode(response.body));
+        } else if (response.statusCode >= 300) {
+          throw 'Failed to load popular movies';
+        }
+      } catch (e) {
+        log(e.toString());
+        await Future.delayed(const Duration(seconds: 1));
+        attempts++;
+      }
+    }
+    throw 'Faled to load popular movies';
   }
 }
